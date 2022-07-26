c02d845a8fd825c8ececc709d9599338a06f72e5
Karina Santos <karinas.santos@terra.com.br>
2022-07-25 21:51:17 -0300

    first commit

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,34 @@
+package br.com.dio.desafio.dominio;
+
+import java.net.ContentHandler;
+
+public class Curso extends Conteudo {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo()+ '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+}
